851a1899dff0ffca75dabe58449062bfd8ea3060
cesar410 <111253945+cesar410@users.noreply.github.com>
2022-09-09 13:19:05 -0600

    Add files via upload

---
+++ cartaDeVinos/cartaDeVinos.js
@@ -0,0 +1,147 @@
+/* funcion de  boton para mostrar ventana emergente 1*/
+function ventana1(){
+    swal({
+        title: "Castillo Ygay, de Marqués Murrieta.!",
+        text: "Precio  $1050.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+        
+    });
+    
+}
+
+/* funcion de  boton para mostrar ventana emergente 2*/
+function ventana2(){
+    swal({
+        title: "Aubert Pinot Noir Sonoma Coast.!",
+        text: "Precio  $700.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+    });
+    
+}
+
+/* funcion de  boton para mostrar ventana emergente 3*/
+function ventana3(){
+    swal({
+        title: "Brunello di Montalcino Lé Lucére.!",
+        text: "Precio  $500.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+    });
+    
+}
+
+
+/* funcion de  boton para mostrar ventana emergente 4*/
+function ventana4(){
+    swal({
+        title: "Cabernet Sauvignon Mount Veeder.!",
+        text: "Precio  $450.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+        
+    });
+    
+}
+
+
+/* funcion de  boton para mostrar ventana emergente 5*/
+function ventana5(){
+    swal({
+        title: "Châteauneuf-du-Pape Les Trois S.!",
+        text: "Precio  $300.00  Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+
+/* funcion de  boton para mostrar ventana emergente 6*/
+function ventana6(){
+    swal({
+        title: "Chardonnay Russian River Valley Vine H V.!",
+        text: "Precio $250.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+
+/* funcion de  boton para mostrar ventana emergente 7*/
+function ventana7(){
+    swal({
+        title: "Massolino Barolo-bodega italiana.!",
+        text: "Precio $780.00 Dolares" + "\n" +"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+
+
+/* funcion de  boton para mostrar ventana emergente 8*/
+function ventana8(){
+    swal({
+        title: "Piedra Negra Chacayes Los Chacayes!",
+        text: "Precio $500.00 Dolares" +"\n"+"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+/* funcion de  boton para mostrar ventana emergente 9*/
+function ventana9(){
+    swal({
+        title: "Pinont Noir Ribbon Ridge the Beaux F V.!",
+        text: "Precio $490.00 Dolares" + "\n"+"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+
+/* funcion de  boton para mostrar ventana emergente 10*/
+function ventana10(){
+    swal({
+        title: "Bollinger Brut Champagne La G A.!",
+        text: "Precio $115.00 Dolares" +"\n"+"Servicio listo para degustar ",
+        icon: "success",
+        button: "visitanos",
+        
+    });
+}
+
+
+/* funcion para decidir
+function ventana6(){
+    swal("Chips crocantes de papa", {
+        buttons: {
+          cancel: "Precio",
+          catch: {
+            text: "Tiempo",
+            value: "tiempo",
+          },
+          
+        },
+      })
+      .then((value) => {
+        switch (value) {
+         
+          case "tiempo":
+            swal("Tiempo", "tiempo aproximado de preparacion 5 minutos!", "success");
+            break;
+       
+          default:
+            swal("Precio", "$7.00 dolares!", "success");
+        }
+      });
+
+}*/
